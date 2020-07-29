a6c7cded63eb3e8d3c87db42c7b37cc692d0544e
robertelias <robert.elias@live.com>
2020-07-29 16:18:23 -0700

    updates

--- gameoflife/src/Game.js
+++ gameoflife/src/Game.js
@@ -1,32 +1,46 @@
 import React from 'react';
 import './Game.css';
 
-const CELL_SIZE = 20;
+const CELL_SIZE = 25;
 const WIDTH = 800;
 const HEIGHT = 600;
 
 //Create Cells
 class Cell extends React.Component {
-  render() {
-    const { x, y} = this.props;
+  render () {
+    const {x, y} = this.props;
     return (
-      <div className = "Cell" style = {{
+      <div
+        className="Cell"
+        style={{
           left: `${CELL_SIZE * x + 1}px`,
           top: `${CELL_SIZE * y + 1}px`,
           width: `${CELL_SIZE - 1}px`,
           height: `${CELL_SIZE - 1}px`,
-      }}/>
+        }}
+      />
     );
   }
 }
 class Game extends React.Component {
+
+  //state management
+  
   render () {
     return (
-      
       <div>
-        {' '}{' '}{' '}{' '}{' '}{' '}{' '}{' '}
-        <div className="Board"  style={{ width: WIDTH, height: HEIGHT,    backgroundSize: `${CELL_SIZE}px ${CELL_SIZE}px`}}></div>
-        {' '}{' '}{' '}{' '}{' '}{' '}
+        <div>
+          {' '}{' '}{' '}{' '}{' '}{' '}{' '}{' '}
+          <div
+            className="Board"
+            style={{
+              width: WIDTH,
+              height: HEIGHT,
+              backgroundSize: `${CELL_SIZE}px ${CELL_SIZE}px`,
+            }}
+          />
+          {' '}{' '}{' '}{' '}{' '}{' '}
+        </div>
       </div>
     );
   }
