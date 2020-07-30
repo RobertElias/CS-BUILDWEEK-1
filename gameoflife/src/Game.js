625c0fe591b7091907e0df673d2a5efde062fbb9
robertelias <robert.elias@live.com>
2020-07-29 21:26:16 -0700

    updates

--- gameoflife/src/Game.js
+++ gameoflife/src/Game.js
@@ -1,25 +1,19 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import './Game.css';
-import { ButtonToolbar, Dropdown, DropdownButton } from "react-bootstrap";
+import "./index.css";
+//import * as ReactBootstrap from 'react-bootstrap';
+import { ButtonToolbar } from "react-bootstrap";
 
-const CELL_SIZE = 25;
-const WIDTH = 800;
-const HEIGHT = 600;
-
-//Create Cells
-class Cell extends React.Component {
-  render () {
-    const {x, y} = this.props;
+class Box extends React.Component {
+  selectBox = () => {
+    this.props.selectBox(this.props.row, this.props.col);
+  };
+  render() {
     return (
       <div
-        className="Cell"
-        style={{
-          left: `${CELL_SIZE * x + 1}px`,
-          top: `${CELL_SIZE * y + 1}px`,
-          width: `${CELL_SIZE - 1}px`,
-          height: `${CELL_SIZE - 1}px`,
-        }}
+        className={this.props.boxContainer}
+        id={this.props.id}
+        onClick={this.selectBox}
       />
     );
   }
