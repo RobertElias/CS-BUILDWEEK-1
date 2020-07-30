524a396389ac85b8ae5233d891b9c72fd2d1a859
robertelias <robert.elias@live.com>
2020-07-29 21:25:10 -0700

    Game.css updated

--- gameoflife/src/Game.js
+++ gameoflife/src/Game.js
@@ -3,126 +3,61 @@ import ReactDOM from "react-dom";
 import './Game.css';
 import { ButtonToolbar, Dropdown, DropdownButton } from "react-bootstrap";
 
-class Box extends React.Component {
-  selectBox = () => {
-    this.props.selectBox(this.props.row, this.props.col);
-  };
-  render() {
+const CELL_SIZE = 25;
+const WIDTH = 800;
+const HEIGHT = 600;
+
+//Create Cells
+class Cell extends React.Component {
+  render () {
+    const {x, y} = this.props;
     return (
       <div
-        className={this.props.boxClass}
-        id={this.props.id}
-        onClick={this.selectBox}
+        className="Cell"
+        style={{
+          left: `${CELL_SIZE * x + 1}px`,
+          top: `${CELL_SIZE * y + 1}px`,
+          width: `${CELL_SIZE - 1}px`,
+          height: `${CELL_SIZE - 1}px`,
+        }}
       />
     );
   }
 }
-class Grid extends React.Component {
-  render() {
-    const width = this.props.cols * 14;
-    var rowsArr = [];
+class Game extends React.Component {
 
-    var boxClass = "";
-    for (var i = 0; i < this.props.rows; i++) {
-      for (var j = 0; j < this.props.cols; j++) {
-        // you can do this with map as well
-        let boxId = i + "_" + j;
-
-        boxClass = this.props.gridFull[i][j] ? "box on" : "box off"; // classes from CSS to see if boxes are on or off
-        rowsArr.push(
-          <Box
-            boxClass={boxClass}
-            key={boxId}
-            boxId={boxId}
-            row={i}
-            col={j}
-            selectBox={this.props.selectBox} // this method is being passed through two components
-          />
-        );
-      }
-    }
-    return (
-      <div className="grid" style={{ width: width }}>
-        {rowsArr}
-      </div>
-    );
-  }
-}
-
-class Buttons extends React.Component {
-  handleSelect = (e) => {
-    this.props.gridSize(e) // this event is going to be coming from the eventKey in the DropdownItem section
-  }
-  render() {
-    return (
-      <div className="center">
-        <ButtonToolbar>
-          <button className="btn" onClick={this.props.playButton}>
-            Play
-          </button>
-          <button className="btn" onClick={this.props.pauseButton}>
-            Pause
-          </button>
-          <button className="btn" onClick={this.props.clear}>
-            Clear
-          </button>
-          <button className="btn" onClick={this.props.slow}>
-            Slow
-          </button>
-          <button className="btn" onClick={this.props.fast}>
-            Fast
-          </button>
-          <button className="btn" onClick={this.props.seed}>
-            Seed
-          </button>
-          <DropdownButton
-            title="Grid Size"
-            id="size-menu"
-            onSelect={this.handleSelect}
-          >
-            <Dropdown.Item eventKey="1">25x25</Dropdown.Item>
-            <Dropdown.Item eventKey="2">50x30</Dropdown.Item>
-            <Dropdown.Item eventKey="3">70x50</Dropdown.Item>
-          </DropdownButton>
-        </ButtonToolbar>
-      </div>
-    );
-  }
-}
-
-class Main extends React.Component {
+  // create the constructor
   constructor() {
     super();
-    this.speed = 100;
-    this.rows = 30;
-    this.cols = 50;
+    this.rows = HEIGHT / CELL_SIZE;
+    this.cols = WIDTH / CELL_SIZE;
 
-    this.state = {
-      generation: 0,
-      gridFull: Array(this.rows)
-        .fill()
-        .map(() => Array(this.cols).fill(false)),
-    };
+    this.board = this.makeEmptyBoard();
+}
+  //state management
+  state = {
+    cells:[],
   }
-  // selected box is changed from false to true from the Array,
-  selectBox = (row, col) => {
-    let gridCopy = arrayClone(this.state.gridFull);
-    gridCopy[row][col] = !gridCopy[row][col]; // will turn box green when clicked, not green when clicked again
-    this.setState({
-      gridFull: gridCopy,
-    });
-  };
-  seed = () => {
-    // random generated squares at game start
-    let gridCopy = arrayClone(this.state.gridFull);
-    for (let i = 0; i < this.rows; i++) {
-      for (let j = 0; j < this.cols; j++) {
-        if (Math.floor(Math.random() * 4) === 1) {
-          gridCopy[i][j] = true;
-        }
-      }
-    }
-    this.setState({
-      gridFull: gridCopy,
-    });
-  };
+
+  
+  render () {
+    return (
+      <div>
+        <div>
+          {' '}{' '}{' '}{' '}{' '}{' '}{' '}{' '}
+          <div
+            className="Board"
+            style={{
+              width: WIDTH,
+              height: HEIGHT,
+              backgroundSize: `${CELL_SIZE}px ${CELL_SIZE}px`,
+            }}
+          />
+          {' '}{' '}{' '}{' '}{' '}{' '}
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Game;
