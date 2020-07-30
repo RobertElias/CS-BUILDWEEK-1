25b252f05d65af273afc909bfc75a21f0af545ee
robertelias <robert.elias@live.com>
2020-07-29 21:28:53 -0700

    updates

--- gameoflife/src/Game.js
+++ gameoflife/src/Game.js
@@ -97,3 +97,26 @@ class Main extends React.Component {
         .map(() => Array(this.cols).fill(false)),
     };
   }
+
+  selectBox = (row, col) => {
+    let gridCopy = arrayClone(this.state.gridFull);
+    gridCopy[row][col] = !gridCopy[row][col]; 
+    this.setState({
+      gridFull: gridCopy,
+    });
+  };
+
+  seed = () => {
+    // random generated squares when game starts
+    let gridCopy = arrayClone(this.state.gridFull);
+    for (let i = 0; i < this.rows; i++) {
+      for (let j = 0; j < this.cols; j++) {
+        if (Math.floor(Math.random() * 4) === 1) {
+          gridCopy[i][j] = true;
+        }
+      }
+    }
+    this.setState({
+      gridFull: gridCopy,
+    });
+  };
