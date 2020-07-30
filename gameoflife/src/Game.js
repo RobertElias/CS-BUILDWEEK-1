810a071f6de61139efb0b4dda108cf702768e679
robertelias <robert.elias@live.com>
2020-07-29 18:40:01 -0700

    grid created

--- gameoflife/src/Game.js
+++ gameoflife/src/Game.js
@@ -1,61 +1,50 @@
-import React from 'react';
+import React from "react";
+import ReactDOM from "react-dom";
 import './Game.css';
+import { ButtonToolbar, Dropdown, DropdownButton } from "react-bootstrap";
 
-const CELL_SIZE = 25;
-const WIDTH = 800;
-const HEIGHT = 600;
-
-//Create Cells
-class Cell extends React.Component {
-  render () {
-    const {x, y} = this.props;
+class Box extends React.Component {
+  selectBox = () => {
+    this.props.selectBox(this.props.row, this.props.col);
+  };
+  render() {
     return (
       <div
-        className="Cell"
-        style={{
-          left: `${CELL_SIZE * x + 1}px`,
-          top: `${CELL_SIZE * y + 1}px`,
-          width: `${CELL_SIZE - 1}px`,
-          height: `${CELL_SIZE - 1}px`,
-        }}
+        className={this.props.boxClass}
+        id={this.props.id}
+        onClick={this.selectBox}
       />
     );
   }
 }
-class Game extends React.Component {
+class Grid extends React.Component {
+  render() {
+    const width = this.props.cols * 14;
+    var rowsArr = [];
 
-  // create the constructor
-  constructor() {
-    super();
-    this.rows = HEIGHT / CELL_SIZE;
-    this.cols = WIDTH / CELL_SIZE;
+    var boxClass = "";
+    for (var i = 0; i < this.props.rows; i++) {
+      for (var j = 0; j < this.props.cols; j++) {
+        // you can do this with map as well
+        let boxId = i + "_" + j;
 
-    this.board = this.makeEmptyBoard();
-}
-  //state management
-  state = {
-    cells:[],
-  }
-
-  
-  render () {
-    return (
-      <div>
-        <div>
-          {' '}{' '}{' '}{' '}{' '}{' '}{' '}{' '}
-          <div
-            className="Board"
-            style={{
-              width: WIDTH,
-              height: HEIGHT,
-              backgroundSize: `${CELL_SIZE}px ${CELL_SIZE}px`,
-            }}
+        boxClass = this.props.gridFull[i][j] ? "box on" : "box off"; // classes from CSS to see if boxes are on or off
+        rowsArr.push(
+          <Box
+            boxClass={boxClass}
+            key={boxId}
+            boxId={boxId}
+            row={i}
+            col={j}
+            selectBox={this.props.selectBox} // this method is being passed through two components
           />
-          {' '}{' '}{' '}{' '}{' '}{' '}
-        </div>
+        );
+      }
+    }
+    return (
+      <div className="grid" style={{ width: width }}>
+        {rowsArr}
       </div>
     );
   }
 }
-
-export default Game;
