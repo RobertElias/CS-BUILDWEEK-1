5d3d2604ee6685e480983f460300522b656e047b
robertelias <robert.elias@live.com>
2020-07-29 18:40:30 -0700

    buttons created

--- gameoflife/src/Game.js
+++ gameoflife/src/Game.js
@@ -48,3 +48,44 @@ class Grid extends React.Component {
     );
   }
 }
+
+class Buttons extends React.Component {
+  handleSelect = (e) => {
+    this.props.gridSize(e) // this event is going to be coming from the eventKey in the DropdownItem section
+  }
+  render() {
+    return (
+      <div className="center">
+        <ButtonToolbar>
+          <button className="btn" onClick={this.props.playButton}>
+            Play
+          </button>
+          <button className="btn" onClick={this.props.pauseButton}>
+            Pause
+          </button>
+          <button className="btn" onClick={this.props.clear}>
+            Clear
+          </button>
+          <button className="btn" onClick={this.props.slow}>
+            Slow
+          </button>
+          <button className="btn" onClick={this.props.fast}>
+            Fast
+          </button>
+          <button className="btn" onClick={this.props.seed}>
+            Seed
+          </button>
+          <DropdownButton
+            title="Grid Size"
+            id="size-menu"
+            onSelect={this.handleSelect}
+          >
+            <Dropdown.Item eventKey="1">25x25</Dropdown.Item>
+            <Dropdown.Item eventKey="2">50x30</Dropdown.Item>
+            <Dropdown.Item eventKey="3">70x50</Dropdown.Item>
+          </DropdownButton>
+        </ButtonToolbar>
+      </div>
+    );
+  }
+}
